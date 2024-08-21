9a7aa71b13cf1e857e1168ce506d8d33ac173659
andra2608 <dileep.andra12@gmail.com>
2024-08-21 14:51:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI in us-west-2
+  ami           = "ami-0522ab6e1ddcc7055" # Amazon Linux 2 AMI in us-west-2
   instance_type = "t2.micro"
 
   tags = {
